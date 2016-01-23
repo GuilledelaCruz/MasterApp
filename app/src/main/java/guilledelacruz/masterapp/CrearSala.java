eec351095636bf4ff36c10e60fed94bc32562f84
GuilledelaCruz <guille.informatica.sevilla@gmail.com>
2016-01-23 18:50:53 +0100

    [master] Main class created and defined, CreateRoom class created, SearchRoom created

---
+++ app/src/main/java/guilledelacruz/masterapp/CrearSala.java
@@ -0,0 +1,15 @@
+package guilledelacruz.masterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearSala extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_sala);
+
+
+    }
+}
--- app/src/main/java/guilledelacruz/masterapp/MainActivity.java
+++ app/src/main/java/guilledelacruz/masterapp/MainActivity.java
@@ -1,7 +1,10 @@
 package guilledelacruz.masterapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        final Button crearsala = (Button) findViewById(R.id.botoncrearsala);
+        final Button buscarsala = (Button) findViewById(R.id.botoncrearsala);
+
+        crearsala.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CrearSala.class);
+                startActivity(intent);
+            }
+        });
+
+        buscarsala.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BuscarSala.class);
+                startActivity(intent);
+            }
+        });
     }
 }
