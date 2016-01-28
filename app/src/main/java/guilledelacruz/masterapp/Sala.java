f82c113f02a4b5cd58e4cfd89f8460a5a3b7f018
GuilledelaCruz <guille.informatica.sevilla@gmail.com>
2016-01-28 11:21:30 +0100

    [master] Fixed text alignment in tablerows of Room class

--- app/src/main/java/guilledelacruz/masterapp/Sala.java
+++ app/src/main/java/guilledelacruz/masterapp/Sala.java
@@ -1,18 +1,14 @@
 package guilledelacruz.masterapp;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,8 +26,6 @@ public class Sala extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sala);
-
-        addTestPlayers();
 
         // get views
         final TextView viewmaster = (TextView) findViewById(R.id.textosalamaster);
@@ -51,13 +45,19 @@ public class Sala extends AppCompatActivity {
 
         // put info on screen
         setTitle(jugadores.size() + "/" + cap + " - " + sala);
-        viewmaster.setText(nombre);
+
+        if (rol.equals("master")){
+            viewmaster.setText(nombre);
+        }else{
+            jugadores.add(nombre);
+        }
 
         if (anon)
             viewanonimo.setText(getResources().getString(R.string.textoanonimo));
         else
             viewanonimo.setText(getResources().getString(R.string.textopublico));
 
+        // prepare list of players
         JugadorAdapter adapter = new JugadorAdapter(this, jugadores.toArray(new String[jugadores.size()]));
         listjugadores.setAdapter(adapter);
 
@@ -73,19 +73,6 @@ public class Sala extends AppCompatActivity {
 
             }
         });
-    }
-
-    private void addTestPlayers(){
-        jugadores.add("1");
-        jugadores.add("12");
-        jugadores.add("123");
-        jugadores.add("1234");
-        jugadores.add("12345");
-        jugadores.add("123456");
-        jugadores.add("1234567");
-        jugadores.add("12345678");
-        jugadores.add("123456789");
-        jugadores.add("1234567890");
     }
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
